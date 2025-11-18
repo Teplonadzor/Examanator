8abc0db76ce191534cf5cd495ae72ba0056762c1
Teplonadzor <123057766+Teplonadzor@users.noreply.github.com>
2025-11-18 18:09:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,12 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text("222-28-89")
-
+    await update.message.reply_text("222-28-89")
 
 if __name__ == "__main__":
-  app = ApplicationBuilder().token(TOKEN).build()
-  app.add_handler(CommandHandler("start", start))
-  app.run_polling()()
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
